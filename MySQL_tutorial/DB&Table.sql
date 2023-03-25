fa16304e71203c77abe379c8ece5b0e15d1df07c
AnuragDarji <127482974+AnuragDarji@users.noreply.github.com>
2023-03-25 19:21:21 +0530

    Update DB&Table.sql

--- MySQL_tutorial/DB&Table.sql
+++ MySQL_tutorial/DB&Table.sql
@@ -6,6 +6,37 @@ CREATE TABLE city(ID INT,CITY_CODE INT,CITY_NAME VARCHAR(50),CITY_STATE VARCHAR(
 
 SHOW TABLES;
 
+/*
+ +-----------------------+
+ | Tables_in_city_master |
+ +-----------------------+
+ | city                  |
+ +-----------------------+
+*/
+
 INSERT INTO city(ID,CITY_CODE,CITY_NAME,CITY_STATE) VALUES (1,1,'AHEMEDABAD','GUJARAT'),(2,18,'GANDHINAGAR','GUJARAT'),(3,5,'SURAT','GUJARAT'),(4,6,'BARODA','GUJARAT');
 
 SELECT * FROM city;
+
+/*
+ +------+-----------+------------+------------+
+ | ID   | CITY_CODE | CITY_NAME  | CITY_STATE |
+ +------+------------------------+------------+
+ |    1 |         1 | AHEMEDABAD | GUJARAT    |
+ |    2 |        18 | GANDHINAGAR| GUJARAT    |
+ |    3 |         5 | SURAT      | GUJARAT    |
+ |    4 |         6 | BARODA     | GUJARAT    |
+ +------+------------+-----------+------------+
+*/
+
+
+/*
+NOTE:
+====================================================
+-- single-line comment
+/*
+multi-line
+comment
+*/
+====================================================
+*/
